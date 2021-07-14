1584670378319eac671a59b56dec2aacc25d9300
ahsanahmedkhan <ahsanahmed.khan@hotmail.com>
2021-07-14 23:40:12 +0500

    add some styles

--- localstorage/app.js
+++ localstorage/app.js
@@ -1,28 +1,34 @@
+
 const userInfo = [
+
     "Ahsan", 
     "Software Engineer",
     "Full Stack Web Application Developer",
     "Karachi",
     "$8000",
     
-
 ];
 
 let info = userInfo.length
 
 let ul = "<ul>";
-for(let i = 0; i<info; i++){
-    ul += "<li>" +userInfo[i] + "</li>";
-}
+
+userInfo.forEach(myfunction);
     ul += "</ul>";
+
+function myfunction(value){
+    ul += "<li>" + value + "</li>"
+}
 
 document.getElementById("info").innerHTML = ul;
 
 const employee = {
+
     fullName: "Ahsan Ahmed khan",
     Designation: "Web Application Developer",
     residence: "Karachi",
     CellPhone: "0000923133489696"
+
 };
 
 document.getElementById("userName").innerHTML = employee.fullName;
