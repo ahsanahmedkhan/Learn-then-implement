da274b64a54bf3642d6e52b618d349c0df8186ca
ahsanahmedkhan <ahsanahmed.khan@hotmail.com>
2021-07-25 23:02:21 +0500

    Display form Values

--- mymovies/app.js
+++ mymovies/app.js
@@ -1,1 +1,29 @@
-console.log('hi');
+//query Selector
+const movieForm = document.querySelector('#movie-form');
+const movieContainer = document.querySelector('#movie-container');
+
+let listItems = [];
+
+//funcations
+function handleFormSubmit(e){
+    e.preventDefault();
+    const moviename = movieForm.querySelector('#moviename').value;
+    const year = movieForm.querySelector('#year').value;
+    const note = movieForm.querySelector('#note').value;
+
+    const newmovie = {
+        moviename,
+        year,
+        note,
+        id: Date.now()
+    }
+
+    console.log(newmovie);
+
+
+}
+
+
+//events listeners
+
+movieForm.addEventListener('submit', handleFormSubmit);
