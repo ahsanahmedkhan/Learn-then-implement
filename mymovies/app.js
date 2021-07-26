bd69965f118d56dc300ff9c0aab9678c6a78ca61
ahsanahmedkhan <ahsanahmed.khan@hotmail.com>
2021-07-26 19:11:16 +0500

    Style customization

--- mymovies/app.js
+++ mymovies/app.js
@@ -17,36 +17,41 @@ function handleFormSubmit(e){
         note,
         id: Date.now()
     }
+
     listItems.push(newmovie);
     e.target.reset();
     displayMovies();
-    console.log(displayMovies);
+    
 }
 
 function displayMovies(){
     const tempString = listItems.map(item => `
-        <div class="col">
-            <divclass="card md-4 rounded-3 shadow-sm border-success">
+        <div class="col mr-3">
+            <div class="card md-4 rounded-3 shadow-sm border-success">
                 <div class="card-header py-3 text-white bg-success border-success">
                     <h4 class="my-0">${item.moviename}</h4>
                 </div>
                 <div class="card-body">
-                    <ul class="text-start">
-                        <li><strong>Year :</strong>${item.moviename}</li>
-                        <li><strong>Year :</strong>${item.year}</li>
-                        ${!item.note.length ? "" : `<li><strong>Year :</strong>${item.note}</li>`}
+                    <ul class="text-start" style="list-style:none;">
+                        <li><strong>ID : </strong>${item.id}</li>
+                        <li><strong>Name : </strong>${item.moviename}</li>
+                        <li><strong>Year : </strong>${item.year}</li>
+                        ${!item.note.length ? "" : `<li><strong>Note : </strong>${item.note}</li>`}
                     </ul>
-                    <button class="btn btn-lg btn-outline-danger" aria-label="Delete ${item.moviename}" value="${item.id}"></button>
+                    <button class="btn btn-outline-success" aria-label="Delete ${item.moviename}" value="${item.id}">Delete Movie</button>
                 </div>
             </div>
-
         </div>
+        
     `).join('');
-    console.log(tempString);
+    movieContainer.innerHTML = tempString;
+    // console.log(tempString);
     
+ 
 }
-console.log("worked");
+
 
 //events listeners
 
 movieForm.addEventListener('submit', handleFormSubmit);
+
