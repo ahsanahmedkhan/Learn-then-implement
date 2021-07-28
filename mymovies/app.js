c94189c6e0be99e751b30948519287d109512c6e
ahsanahmedkhan <ahsanahmed.khan@hotmail.com>
2021-07-29 04:18:23 +0500

    add DomPurify to protect app from  web scrapping

--- mymovies/app.js
+++ mymovies/app.js
@@ -1,15 +1,17 @@
 //query Selector
+
 const movieForm = document.querySelector('#movie-form');
 const movieContainer = document.querySelector('#movie-container');
 
 let listItems = [];
 
 //funcations
+
 function handleFormSubmit(e){
     e.preventDefault();
-    const moviename = movieForm.querySelector('#moviename').value;
-    const year = movieForm.querySelector('#year').value;
-    const note = movieForm.querySelector('#note').value;
+    const moviename = DOMPurify.sanitize(movieForm.querySelector('#moviename').value);
+    const year = DOMPurify.sanitize(movieForm.querySelector('#year').value);
+    const note = DOMPurify.sanitize(movieForm.querySelector('#note').value);
 
     const newmovie = {
         moviename,
@@ -26,7 +28,7 @@ function handleFormSubmit(e){
 
 function displayMovies(){
     const tempString = listItems.map(item => `
-        <div class="col mr-3">
+        <div class="col mr-3 mb-2">
             <div class="card md-4 rounded-3 shadow-sm border-success">
                 <div class="card-header py-3 text-white bg-success border-success">
                     <h4 class="my-0">${item.moviename}</h4>
@@ -38,20 +40,20 @@ function displayMovies(){
                         <li><strong>Year : </strong>${item.year}</li>
                         ${!item.note.length ? "" : `<li><strong>Note : </strong>${item.note}</li>`}
                     </ul>
-                    <button class="btn btn-outline-success" aria-label="Delete ${item.moviename}" value="${item.id}">Delete Movie</button>
+             <button class="btn btn-outline-success" aria-label="Delete ${item.moviename}" value="${item.id}">Delete Movie</button>
                 </div>
             </div>
         </div>
         
     `).join('');
+
     movieContainer.innerHTML = tempString;
     // console.log(tempString);
-    
- 
 }
 
 
 //events listeners
 
 movieForm.addEventListener('submit', handleFormSubmit);
+movieContainer.addEventListener('refreshMovies',displayMovies);
 
